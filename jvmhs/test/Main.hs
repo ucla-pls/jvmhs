553d5270e3e0288a53d9238e984448d2253a5d25
Christian Gram Kalhauge <christian@kalhauge.dk>
2019-10-07 17:36:37 -0700

    Try to fixup tests

--- jvmhs/test/Main.hs
+++ jvmhs/test/Main.hs
@@ -5,9 +5,4 @@ import Test.Hspec.Formatters
 import qualified Spec
 
 main :: IO ()
-main = hspecWith defaultConfig
-  { configFastFail = True
-  , configFormatter = Just progress
-  , configFailureReport = Just "failures.txt"
-  , configRerun = True
-  } Spec.spec
+main = hspecWith defaultConfig Spec.spec
