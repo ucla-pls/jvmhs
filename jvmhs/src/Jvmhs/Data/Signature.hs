b91f96d5717be7f6144beaf0551d899f1a3f4877
Christian Gram Kalhauge <christian@kalhauge.dk>
2019-10-29 10:58:02 -0700

    Offloaded signature code to jvm-binary

--- jvmhs/src/Jvmhs/Data/Signature.hs
+++ jvmhs/src/Jvmhs/Data/Signature.hs
@@ -19,16 +19,10 @@ simple ClassName.
 
 module Jvmhs.Data.Signature
   ( classTypeName
-  , classTypeFromName
-  , throwsSignatureFromName
   , throwsSignatureName
-  , typeSignatureFromType
 
   , _ThrowsClass
   , _ThrowsTypeVariable
-
-
-  , isSimpleMethodSignature
 
   , module Language.JVM.Attribute.Signature
   ) where
@@ -40,7 +34,6 @@ import Data.Aeson
 import Control.Lens
 
 -- text
-import qualified Data.Text as Text
 import Data.Text.Lazy.Builder as Text
 import qualified Data.Text.Lazy as LazyText
 
@@ -54,70 +47,11 @@ makePrisms ''ThrowsSignature
 
 -- | A classType getter from a ClassType
 classTypeName :: Getting f ClassType ClassName
-classTypeName = Control.Lens.to getClassTypeName
-
-getClassTypeName :: ClassType -> ClassName
-getClassTypeName =
-  (review fullyQualifiedName . Text.intercalate "$" . reverse . getClassName)
-  where
-    getClassName = \case
-      InnerClassType {..} ->
-        ctsInnerClassName : getClassName ctsOuterClassType
-      ClassType {..} ->
-        [ctsClassName ^. fullyQualifiedName]
-
-classTypeFromName :: ClassName -> ClassType
-classTypeFromName cn =
-  -- Note the language is wierd here! Main.A is not Main$A, but Main<T>.A is!
-  ClassType cn []
-
-throwsSignatureFromName :: ClassName -> ThrowsSignature
-throwsSignatureFromName cn =
-  ThrowsClass (classTypeFromName cn)
+classTypeName = Control.Lens.to classTypeToName
 
 throwsSignatureName :: Monoid f => Getting f ThrowsSignature ClassName
 throwsSignatureName =
   _ThrowsClass . classTypeName
-
-referenceTypeFromRefType :: JRefType -> ReferenceType
-referenceTypeFromRefType = \case
-  JTArray a -> RefArrayType (typeSignatureFromType a)
-  JTClass a -> RefClassType (classTypeFromName a)
-
-typeSignatureFromType :: JType -> TypeSignature
-typeSignatureFromType = \case
-  JTBase a -> BaseType a
-  JTRef a  -> ReferenceType (referenceTypeFromRefType a)
-
-isSimpleMethodSignature :: MethodSignature -> Bool
-isSimpleMethodSignature MethodSignature {..} = and
-  [ null msTypeParameters
-  , all isSimpleTypeSignature msArguments
-  , all isSimpleTypeSignature msResults
-  , all isSimpleThrowsSignature msThrows
-  ]
-
-isSimpleTypeSignature :: TypeSignature -> Bool
-isSimpleTypeSignature = \case
-  BaseType _ -> True
-  ReferenceType a -> isSimpleReferenceType a
-
-isSimpleReferenceType :: ReferenceType -> Bool
-isSimpleReferenceType = \case
-  RefArrayType a -> isSimpleTypeSignature a
-  RefClassType a -> isSimpleClassType a
-  RefTypeVariable _ -> False
-
-isSimpleClassType :: ClassType -> Bool
-isSimpleClassType = \case
-  ClassType _ [] -> True
-  _ -> False
-
-isSimpleThrowsSignature :: ThrowsSignature -> Bool
-isSimpleThrowsSignature = \case
-  ThrowsClass a -> isSimpleClassType a
-  ThrowsTypeVariable _ -> False
-
 
 instance ToJSON (ClassSignature) where
   toJSON = String . classSignatureToText
