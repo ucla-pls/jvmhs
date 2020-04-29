26df81cb9be2b49a9b24b69aefaa09d085b68ad5
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-04-29 10:42:51 +0200

    Update jvm-binary to better nix

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 { pkgs ? import ./nix/nixpkgs.nix {}
 , compiler ? "default"
+, jvm-binary ? import ./nix/jvm-binary.nix
 }: 
 let 
   haskellPackages = 
@@ -8,17 +9,12 @@ let
     else pkgs.haskell.packages."${compiler}";
 in
   haskellPackages.developPackage {
-    root = ./.;
+    root = bulitins.filterSource 
+      (path: type: baseNameOf path != ".nix")
+      ./.;
     name = "jvmhs";
-    source-overrides = {
-      reduce = ../reduce;
-    };
+    source-overrides = { inherit jvm-binary; };
     overrides = hsuper: hself: {
-      dirtree = haskellPackages.callHackageDirect { 
-        pkg = "dirtree";
-        ver = "0.1.3";
-        sha256 = "sha256:0kl31l2ip856saq5lhfjr4wv04i2pyj6sf3lkzk9bj7w1m9v0klz";
-      } {};
     };
     modifier = drv:
       with pkgs.haskell.lib;
