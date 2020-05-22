8f3e7b116147d9aec81660e77d10d2c8108fb147
Christian Gram Kalhauge <kalhauge@cs.ucla.edu>
2020-05-22 21:52:30 +0200

    Fix JSON

--- src/Jvmhs/Format/Json.hs
+++ src/Jvmhs/Format/Json.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE EmptyCase #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS -fno-warn-orphans #-}
 {-# OPTIONS -fno-warn-unused-imports #-}
 module Jvmhs.Format.Json where
+
+-- base
+import Data.Foldable
 
 -- aeson
 import           Data.Aeson
 
 -- lens
-import           Control.Lens
+import           Control.Lens hiding ((.=))
 
 -- containers
 import qualified Data.Set                      as Set
@@ -15,8 +21,12 @@ import qualified Data.Set                      as Set
 -- text
 import qualified Data.Text                     as Text
 
+-- jvm-binary
+import qualified Language.JVM as JVM
+
 -- jvmhs
 import           Jvmhs.Data.Class
+import           Jvmhs.Data.Type
 import           Jvmhs.Data.Identifier
 
 {-
@@ -25,5 +35,53 @@ How do we format json?
 
 -}
 
+instance ToJSON a => ToJSON (Annotated a) where
+  toJSON (Annotated a annos) = case annos of 
+    [] -> toJSON a
+    _ -> Object $ 
+      "content" .= a <> "annotations" .= annos
+
 instance ToJSON Class where
-  toJSON _ = String "class"
+  toJSON c = Object $ fold
+    [ "name" .= (c ^. className)
+    , "access" .= (c ^. classAccessFlags)
+    , "type_param" .= (c ^. classTypeParameters)
+    , "super" .= (c ^. classSuper)
+    , "interfaces" .= (c ^. classInterfaces)
+    , "fields" .= (c ^. classFields)
+    , "methods" .= (c ^. classMethods)
+    , "bootstrap_methods" .= (c ^. classBootstrapMethods)
+    , "enclosing" .= case c ^. classEnclosingMethod of
+        Just (cls,mm) ->  Object ("class" .= cls <> "method" .= mm)
+        Nothing -> Null
+    , "inner_classes" .= (c ^. classInnerClasses)
+    , "annotations" .= (c ^. classAnnotations)
+    , "version" .= (c ^. classVersion)
+    ]
+
+instance ToJSON Field 
+
+instance ToJSON Method 
+
+instance ToJSON BootstrapMethod 
+
+instance ToJSON ReturnType 
+instance ToJSON Parameter 
+instance ToJSON PAccessFlag 
+
+instance ToJSON TypeParameter 
+instance ToJSON ThrowsType 
+instance ToJSON TypeVariableName 
+instance ToJSON TypeVariable 
+instance ToJSON ClassType
+instance ToJSON TypeArgument
+instance ToJSON ReferenceType
+instance ToJSON ArrayType
+instance ToJSON Type
+instance ToJSON JBaseType
+instance ToJSON InnerClass
+
+instance ToJSON Annotation
+instance ToJSON AnnotationValue
+instance ToJSON (EnumValue JVM.High)
+
