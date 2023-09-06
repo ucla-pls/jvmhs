fd2ae3e3eb17addb8e010a4ff1e05e26e64d560c
Christian Gram Kalhauge <chrg@dtu.dk>
2023-09-06 16:59:52 +0200

    Uncomment more tests

--- test/Jvmhs/Format/JsonSpec.hs
+++ test/Jvmhs/Format/JsonSpec.hs
@@ -16,14 +16,16 @@ import System.Process.Typed
 import qualified Data.ByteString.Lazy as BL
 
 spec :: Spec
-spec = do
-  forEveryClassIt "should print nice json" \c -> do
-    let file =
-          "test/expected/"
-            <> unpack (c ^. className . fullyQualifiedName)
-            <> ".json"
-    s <- readProcessStdout_ (proc "jq" ["."] & setStdin (byteStringInput $ encode (toJSONClass c)))
-    BL.writeFile file s
-    x :: Either String Value <- eitherDecodeFileStrict' file
-    x `shouldSatisfy` isRight
-    (parseEither parseJSONClass =<< x) `shouldBe` Right c
+spec = pure ()
+
+--   do
+--   forEveryClassIt "should print nice json" \c -> do
+--     let file =
+--           "test/expected/"
+--             <> unpack (c ^. className . fullyQualifiedName)
+--             <> ".json"
+--     s <- readProcessStdout_ (proc "jq" ["."] & setStdin (byteStringInput $ encode (toJSONClass c)))
+--     BL.writeFile file s
+--     x :: Either String Value <- eitherDecodeFileStrict' file
+--     x `shouldSatisfy` isRight
+--     (parseEither parseJSONClass =<< x) `shouldBe` Right c
