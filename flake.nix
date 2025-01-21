21bc0501ba506f020e878d83c991ba50c5ba0a1a
Christian Gram Kalhauge <chrg@dtu.dk>
2025-01-21 15:54:18 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,61 +1,72 @@
 {
   description = "A library for reading Java class-files";
 
-  inputs =
-    {
-      nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-      flake-utils.url = "github:numtide/flake-utils";
-      jvm-binary.url = "github:ucla-pls/jvm-binary";
-      cones.url = "github:kalhauge/cones";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    let
-      packages = p: {
-        "jvmhs" = p.callCabal2nixWithOptions "jvmhs" self "" { };
-        "jvm-binary" = p.callCabal2nixWithOptions "jvm-binary" inputs.jvm-binary "" { };
-      };
-      overlays = final: prev: {
-        haskellPackages = prev.haskellPackages.extend (p: _: packages p);
-        jvm2json = final.haskell.lib.justStaticExecutables final.haskellPackages.jvmhs;
-      };
-    in
+    jvm-binary = {
+      url = "github:ucla-pls/jvm-binary";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    cones = {
+      url = "github:kalhauge/cones";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    packages = p: {
+      "jvmhs" = p.callCabal2nixWithOptions "jvmhs" self "" {};
+      "jvm-binary" = p.callCabal2nixWithOptions "jvm-binary" inputs.jvm-binary "" {};
+    };
+    overlays = final: prev: {
+      haskellPackages = prev.haskellPackages.extend (p: _: packages p);
+      jvm2json = final.haskell.lib.justStaticExecutables final.haskellPackages.jvmhs;
+    };
+  in
     {
       overlays = {
         default = overlays;
-        all = [ overlays inputs.cones.overlays.default ];
+        all = [overlays inputs.cones.overlays.default];
       };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs
-          {
-            inherit system;
-            overlays = [ overlays inputs.cones.overlays.default ];
-          };
-        hpkgs = pkgs.haskellPackages;
-      in
-      {
-        packages = {
-          default = hpkgs.jvmhs;
-          inherit (hpkgs) jvmhs;
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs =
+        import nixpkgs
+        {
+          inherit system;
+          overlays = [overlays inputs.cones.overlays.default];
         };
-        devShells =
-          let
-            buildInputs = with hpkgs; [
-              cabal-install
-              ghcid
-              haskell-language-server
-              hpack
-              fourmolu
-            ];
-            withHoogle = true;
-          in
+      hpkgs = pkgs.haskellPackages;
+    in {
+      packages = {
+        default = hpkgs.jvmhs;
+        inherit (hpkgs) jvmhs;
+      };
+      devShells = let
+        buildInputs = with hpkgs; [
+          cabal-install
+          ghcid
+          haskell-language-server
+          hpack
+          fourmolu
+        ];
+        withHoogle = true;
+      in {
+        default =
+          hpkgs.shellFor
           {
-            default = hpkgs.shellFor
-              {
-                name = "jvmhs-shell";
-                packages = p: [ p.jvmhs ];
-                inherit buildInputs withHoogle;
-              };
+            name = "jvmhs-shell";
+            packages = p: [p.jvmhs];
+            inherit buildInputs withHoogle;
           };
-      });
+      };
+    });
 }
